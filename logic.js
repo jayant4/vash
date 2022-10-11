bc288e71bc0bdfeb27cac11fadd03bcc3ef2c69f
jayant4 <jsarkar141143@gmail.com>
2022-10-11 17:55:39 +0530

    data from url working

---
+++ logic.js
@@ -0,0 +1,8 @@
+module.exports = function updateBook(title, books,price) {
+
+    console.log("--------->",title,books);
+   let index = books.findIndex((obj => obj.title === title));
+   console.log(index);
+   books[index].price = 40
+
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const port = 3000
 app.set('view engine', 'vash');
+const updateBook = require("./logic")
 
 app.get("/home", (request, response) => {
     response.render('home')
@@ -11,20 +12,34 @@ app.get("/about", (request, response) => {
     response.render('about')
 })
 
-const books = [ 
-    { id:1, title : 'T1', author : 'A1', price : 1000 },
-    { id:2,title : 'T1', author : 'A1', price : 1000 },
-    { id:3,title : 'T1', author : 'A1', price : 1000 },
-    { id:4,title : 'T1', author : 'A1', price : 1000 },
-    { id:5,title : 'T1', author : 'A1', price : 1000 },
+const books = [
+    { id: 1, title: 'T1', author: 'A1', price: 1000 },
+    { id: 2, title: 'T2', author: 'A2', price: 1000 },
+    { id: 3, title: 'T3', author: 'A3', price: 1000 },
+    { id: 4, title: 'T4', author: 'A4', price: 1000 },
+    { id: 5, title: 'T5', author: 'A5', price: 1000 },
 ]
-app.get("/books", (request,response) =>{
-    response.render("books", { books } )
+
+app.get("/books", (request, response) => {
+    response.render("books", { books })
 })
-app.get("/books", (request,response) =>{
-    response.render("books", { books } )
+
+
+app.get("/books/edit", (request, response) => {
+    const title = request.query.title
+    // console.log(title);
+    // updateBook(title,books);
+    // console.log(books);
+    response.render("editBook", { books })
 })
-  
+app.get("/books/update", (request, response) => {
+    const title = request.query.title
+    console.log(title);
+    updateBook(title, books);
+    console.log(books);
+    response.render("editBook", { title })
+})
+
 app.get("/contact", (request, response) => {
     response.render('contact')
 })
